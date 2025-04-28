5f87bf3ecf9a56bcd7b12310311f0d0a4e835a2c
DA317 <keehong11@naver.com>
2025-04-28 10:34:07 +0900

    Learning C Language is awesome

---
+++ 혼공C_예제소스/1-2장/2-2/예제2-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>         // stdio : standard input output(표준입출력)의 줄임말 : c언어에서 기본으로 사용하는 입출력 함수가 들어있음. 
+
+int main(void)
+{
+	printf("Be happy");    // 문자열 "Be happy" 출력
+	printf("My friend");   // 문자열 "My friend" 출력
+
+	return 0;
+}
+
+/* printf : print formatted 라는 뜻.
+*/
