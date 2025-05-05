08eaa8cf2a3310d5f95fdfd47bae8c112d851b9f
DA317 <keehong11@naver.com>
2025-05-05 15:41:57 +0900

    250505 예제 4-2 완료

--- 혼공C_예제소스/4장/4-2/예제4-2.c
+++ 혼공C_예제소스/4장/4-2/예제4-2.c
@@ -1,3 +1,4 @@
+// 나누기 연산자 / 와 나머지 연산자 %
 #include <stdio.h>
 
 int main(void)
@@ -10,9 +11,12 @@ int main(void)
 	banana = 5 / 2;			// 정수와 정수의 나누기 연산
 	orange = 5 % 2;			// 정수와 정수의 나머지 연산(%)
 
-	printf("apple : %.1lf\n", apple);
-	printf("banana : %d\n", banana);
-	printf("orange : %d\n", orange);
+	printf("apple : %.1lf\n", apple); // 2.5
+	printf("banana : %d\n", banana); // 2
+	printf("orange : %d\n", orange); // 1
 
 	return 0;
 }
+
+// 나누기 연산자, 나머지 연산자 
+// 설명이랄게 없다. 외워야 함. 
