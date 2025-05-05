87d6c7d9c33f2f7fdfac2e552f5776b8800c0615
DA317 <keehong11@naver.com>
2025-05-05 14:35:14 +0900

    250505 예제 3-7 완료

--- 혼공C_예제소스/3장/3-6/예제3-6.c
+++ 혼공C_예제소스/3장/3-6/예제3-6.c
@@ -2,10 +2,13 @@
 
 int main(void)
 {
-	char fruit[20] = "strawberry";               // char 배열 선언과 문자열 초기화
+	char fruit[20] = "strawberry";               // char 배열 선언과 문자열 초기화 ex) char 배열명[문자열길이+1] = 문자열
 	
-	printf("딸기 : %s\n", fruit);                // 배열명으로 저장된 문자열 출력
-	printf("딸기쨈 : %s %s\n", fruit, "jam");    // 문자열 상수를 직접 %s로 출력
+	printf("딸기 : %s\n", fruit);                // 배열명으로 저장된 문자열 출력 = 딸기 : strawberry
+	printf("딸기쨈 : %s %s\n", fruit, "jam");    // 문자열 상수를 직접 %s로 출력 = 딸기쨈 : strawberry jam
 
 	return 0;
 }
+
+// 5행에서 strawberry 는 10자이므로 fruit 배열의 크기는 최소한 11보다 커야 함. 
+// 이런 char 배열은 문자열을 저장하는 변수의 역할을 하며, %s 출력함. 
