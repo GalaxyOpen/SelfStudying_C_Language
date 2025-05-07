d73e3cecf744a3ca7328052d300e912afb6ae866
GalaxyOpen <keehong11@naver.com>
2025-05-07 21:19:39 +0900

    250507 예제 5-6 완료 (switch case default break)

--- 혼공C_예제소스/5장/5-6/예제5-6.c
+++ 혼공C_예제소스/5장/5-6/예제5-6.c
@@ -24,3 +24,15 @@ int main(void)
 
 	return 0;
 }
+
+// switch case 구문 
+// 첫 5행에서 rank에 2값을 입력. m=0 
+// case 1: -> rank라는 변수에 들어가 있는 값이 1이면 
+// m = 300 -> 0이었던 m 값을 300으로 바꾸고 break : switch 구문 밖으로 이동. 
+// case 2: -> rank라는 변수에 들어가 있는 값이 2면 
+// m = 200 -> 0이었던 m 값을 200으로 바꾸고 break : switch 구문 밖으로 이동. 
+// case 3: -> rank라는 변수에 들어가 있는 값이 3이면
+// m = 100 -> 0이었던 m 값을 100으로 바꾸고 break : switch 구문 밖으로 이동. 
+// default : 위의 모든 case에 해당하는 값이 없다면 
+// m = 10 -> 0이었던 m 값을 10으로 바꾸고 break : switch 구문 밖으로 이동. 
+// m : 200 이 출력됨.  
