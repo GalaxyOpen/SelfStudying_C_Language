bc71a5646ade5937c23ad42a04c26b3372aebefd
GalaxyOpen <keehong11@naver.com>
2025-05-07 21:03:49 +0900

    250507 예제 5-5 완료

--- 혼공C_예제소스/5장/5-5/예제5-5.c
+++ 혼공C_예제소스/5장/5-5/예제5-5.c
@@ -18,3 +18,10 @@ int main(void)
 
 	return 0;
 }
+
+// if in if and else 
+// 1. 변수 int a = 10, b = 20 선언 
+// 2. 첫 조건 if a < 0 이 참 이면서 
+// 2-1. b가 0보다 크면 ok 출력. 
+// 3. 첫 조건 if a < 0 이 거짓 이면 ok 출력. 
+ 
