6476381e646b147f1647c170b459fda32fd9e4bb
DA317 <keehong11@naver.com>
2025-05-15 17:13:45 +0900

    250515 예제 8-3 (sizeof 연산자를 활용한 배열처리)

--- 혼공C_예제소스/8장/8-3/예제8-3.c
+++ 혼공C_예제소스/8장/8-3/예제8-3.c
@@ -31,3 +31,12 @@ int main(void)
 
 	return 0;
 }
+
+// sizeof 연산자를 활용한 배열 처리 
+
+// sizeof(배열명), / sizeof(배열 요소)
+// count = sizeof(score) / sizeof(score[0]); << 여기서 count는 배열 요소의 개수(5개), sizeof(score)는 배열 전체 크기(20바이트), 
+// sizeof(score[0]) 는 배열 요소 하나의 크기(4바이트)를 의미한다. 결과인 count 에서는 20 / 4 값인 5가 들어가게 된다. 
+
+// 22행에서 배열의 평균을 구할 때도 사용함. 단, 이때는 total과 count가 모두 int형이므로
+// 소수점 이하까지 구하려면 형 변환 연산자인 (double)을 사용해서 실수 연산이 되도록 했다. 
