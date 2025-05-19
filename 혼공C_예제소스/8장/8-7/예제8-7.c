e48cac6a1691db4293eb48f276b0ef6194cc5730
GalxyOpen <keehong11@naver.com>
2025-05-20 00:31:34 +0900

    250520 예제 8-7 완료 (간단 예제)

--- 혼공C_예제소스/8장/8-7/예제8-7.c
+++ 혼공C_예제소스/8장/8-7/예제8-7.c
@@ -2,11 +2,11 @@
 
 int main(void)
 {
-	char str[5];
+	char str[5]; // 5행 쓰레기 값이 남아 있음. 
 
-	str[0] = 'O';
-	str[1] = 'K';
-	printf("%s\n", str);
+	str[0] = 'O'; // O 값으로 초기화 
+	str[1] = 'K'; // K 값으로 초기화 
+	printf("%s\n", str); // OK 값 이후에 나타나면 빈 값이 있기 때문에 그 값들은 쓰레기 값이 출력됨. 
 
 	return 0;
 }
